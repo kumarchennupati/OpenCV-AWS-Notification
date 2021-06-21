47bf7d010e9947b659f1225f68d32b0c001af643
Mandepudi Nobel Chowdary <nobelchowdary@gmail.com>
2021-06-21 13:13:18 +0530

    Delete mypass - Copy.py

--- mypass - Copy.py
+++
@@ -1,6 +0,0 @@
-twilio_sid = 'xxxxxxxxxxxxxxxxxxxxxxxx'
-twilio_auth_token = 'xxxxxxxxxxxxxxxxxxxxx'
-mail_pass = 'xxxxxxxxxxx'
-phone_number = ['+91xxxxxxxxxxx']
-from_mail = 'xxxxxxxxxxxxx@gmail.com'
-to_mail = ['xxxxxxxxxxxxxxxxx@gmail.com']
